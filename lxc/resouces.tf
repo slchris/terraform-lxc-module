75439f1db3b62ffa0370c9c9ce822887e2e8a475
Chris Su <chris@lesscrowds.org>
2023-07-25 23:43:58 +0800

    add: s3 backend support

--- lxc/resouces.tf
+++ lxc/resouces.tf
@@ -5,6 +5,18 @@ terraform {
       version = "1.9.1"
     }
   }
+  backend "s3" {
+    bucket = var.bucket
+    key = "terraform.tfstate"
+    endpoint = var.s3_endpoint
+    access_key= var.s3_access_key
+    secret_key= var.s3_secret_key
+    region = "main"
+    skip_credentials_validation = false
+    skip_metadata_api_check = true
+    skip_region_validation = true
+    force_path_style = true
+  }
 }
 
 
--- lxc/variables.tf
+++ lxc/variables.tf
@@ -3,63 +3,81 @@
 variable "lxc_addresses" {
   default     = "192.168.100.2"
   description = "host ip of the lxc server"
-  type        = string
+  type  = string
 }
 
 variable "lxc_port" {
   default     = "8443"
   description = "port of the lxc server"
-  type        = string
+  type  = string
 }
 
 
 
 variable "lxc_password" {
   description = "password of the lxc server"
-  type        = string
+  type = string
 }
 
 
 
 variable "lxc_name" {
   description = "lxc container name"
-  type        = string
+  type = string
 }
 
 variable "lxc_image" {
   description = "lxc image name"
-  type        = string
+  type = string
 }
 
 
 
 variable "lxc_ephemeral" {
   description = "lxc ephemeral name"
-  type        = bool
+  type = bool
 }
 
 
 variable "lxc_type" {
   description = "lxc type name"
-  type        = string
+  type = string
 }
 
 variable "lxc_autostart" {
   description = "lxc autostart name"
-  type        = bool
+  type = bool
 }
 
 
 
 variable "lxc_cpu" {
   description = "lxc cpu limitis"
-  type        = number
+  type = number
 }
 
 variable "lxc_memory" {
   description = "lxc memory limitis"
-  type        = string
+  type = string
+}
+
+variable "bucket" {
+  description = "s3 bucket"
+  type = string
 }
 
 
+variable "s3_endpoint" {
+  description = "s3 backend"
+  type = string
+}
 
+variable "s3_access_key" {
+  description = "s3 access key"
+  type = string
+}
+
+variable "s3_secret_key" {
+  description = "s3 secret key"
+  type = string
+}
